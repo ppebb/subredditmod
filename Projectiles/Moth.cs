844f6a7bda2739f32f7f57ab8f9228c8876068fe
pollen__ <36719558+pollen00@users.noreply.github.com>
2020-08-21 11:23:55 -0500

    Fixed moth not animating

--- Projectiles/Moth.cs
+++ Projectiles/Moth.cs
@@ -1,4 +1,4 @@
-﻿using Terraria;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -16,7 +16,13 @@ namespace rterrariamod.Projectiles
             projectile.alpha = 255;
             projectile.timeLeft = 600;
             projectile.extraUpdates = 3;
+            Main.projFrames[projectile.type] = 4;
             aiType = ProjectileID.Bee;
+        }
+
+        public override void AI()
+        {
+            if ((projectile.frameCounter = ++projectile.frameCounter % 1) == 0) projectile.frame = ++projectile.frame % 5;
         }
     }
 }

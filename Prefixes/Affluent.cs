c3cc26bf6928c7aeca91ad05a8868c53a1d2d1a7
pollen__ <36719558+pollen00@users.noreply.github.com>
2020-08-21 10:54:34 -0500

    Removed comments

--- Prefixes/Affluent.cs
+++ Prefixes/Affluent.cs
@@ -5,27 +5,22 @@ namespace rterrariamod.Prefixes
 {
     public class Affluent : ModPrefix
     {
-        public override float RollChance(Item item)
-        // Chance to roll this prefix   
+        public override float RollChance(Item item)  
             => 1f;
 
         public override bool CanRoll(Item item)
-        // Can you get this prefix?
             => true;
 
         public override PrefixCategory Category
-        // Only for accessories
             => PrefixCategory.Accessory;
 
         public override void ModifyValue(ref float valueMult)
         {
-            // Modifies value of item after prefix
             valueMult *= 1.45f;
         }
 
         public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
         {
-            // Gives more coins from enemies
             Player player = Main.LocalPlayer;
             player.coins = true;
         }
